2d1d5a6470f3fc3279019e9fc0f2d74cc2bc85ec
pmarangone <patrickmarangone@gmail.com>
2022-05-15 12:46:26 -0300

    added server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+#[macro_use]
+extern crate rocket;
+
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 
@@ -8,23 +11,42 @@ pub struct TokenInfo {
     symbol: String,
 }
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let example: String = String::from("https://httpbin.org/ip");
+// #[tokio::main]
+async fn get_response() -> Result<String, Box<dyn std::error::Error>> {
     let path: String = String::from("https://testnet-indexer.ref-finance.com/list-token-price");
 
     let resp: HashMap<String, TokenInfo> = reqwest::get(path).await?.json().await?;
     println!("{:#?}", resp);
 
-    Ok(())
+    let mut res: String = String::from("Failed");
+
+    for (key, value) in resp.iter() {
+        res = value.symbol.clone();
+        break;
+    }
+
+    println!("The sample will be {}", res);
+
+    Ok(res)
 }
 
-// #[tokio::main]
-// async fn main() -> Result<(), Box<dyn std::error::Error>> {
-//     let example: String = String::from("https://httpbin.org/ip");
-//     let path: String = String::from("https://testnet-indexer.ref-finance.com/list-token-price");
+#[get("/url")]
+pub async fn get_url() -> String {
+    let val = get_response().await;
 
-//     let body = reqwest::get(path).await?.text().await?;
-//     println!("body = {:?}", body);
-//     Ok(())
-// }
+    let mut value: String = String::from("Failed");
+
+    match val {
+        Ok(x) => value = x,
+        _ => println!("Error!"),
+    }
+
+    println!("{}", value);
+
+    String::from("")
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/hello", routes![get_url])
+}
